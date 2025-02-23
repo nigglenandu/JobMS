e2215b5e2c4a99dc74858fdbe21234a92ddef992
[Niggle Nandu] <nigglenandu@gmail.com>
2025-02-23 17:00:30 +0545

    eureka jobms and service reg

--- companyms/src/main/java/com/FJobMS/companyms/Company/CompanyController.java
+++ companyms/src/main/java/com/FJobMS/companyms/Company/CompanyController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class CompanyController {
+public class  CompanyController {
 
     @Autowired
     private ICompanyService service;
