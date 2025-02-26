0313441e9d3d133b5e3f0e565b478913ee8c8a85
[Niggle Nandu] <nigglenandu@gmail.com>
2025-02-26 17:40:53 +0545

    company and review register to eureka

--- reviewms/src/main/java/com/FJobMS/reviewms/ReviewmsApplication.java
+++ reviewms/src/main/java/com/FJobMS/reviewms/ReviewmsApplication.java
@@ -2,8 +2,10 @@ package com.FJobMS.reviewms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ReviewmsApplication {
 
 	public static void main(String[] args) {
